81912e17fca3770ced073c00f88457bb203c9fab
Vanaf1979 <vanaf1979@gmail.com>
2025-08-12 14:05:13 +0200

    Added desaturation scripts

--- desaturate_basic.py
+++ desaturate_basic.py
@@ -5,7 +5,7 @@ image_in = Image("./images/checkout.png")
 for pixel in image_in.data():
 
     # A basic desaturation averages the rbg values and stores it in all three channels.
-    # This is optically incorrect. See destatura_optically.py for a correct version.
+    # This is optically incorrect. See desaturate_optically.py for a correct version.
     average = (pixel.r + pixel.g + pixel.b) / 3
     pixel.r = average
     pixel.g = average
