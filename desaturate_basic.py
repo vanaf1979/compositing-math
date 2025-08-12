93d869579874005afb8e875904608a8e94cc51c5
Vanaf1979 <vanaf1979@gmail.com>
2025-08-12 14:03:07 +0200

    Added desaturation scripts

---
+++ desaturate_basic.py
@@ -0,0 +1,15 @@
+from inc.image_classes import Image
+
+image_in = Image("./images/checkout.png")
+
+for pixel in image_in.data():
+
+    # A basic desaturation averages the rbg values and stores it in all three channels.
+    # This is optically incorrect. See destatura_optically.py for a correct version.
+    average = (pixel.r + pixel.g + pixel.b) / 3
+    pixel.r = average
+    pixel.g = average
+    pixel.b = average
+
+
+image_in.show(title="Screen Blend Mode")
