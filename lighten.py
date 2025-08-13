673acc113f6616894cb1fd7d27ede08de93dbc23
Vanaf1979 <vanaf1979@gmail.com>
2025-08-13 16:06:53 +0200

    added premultiplu and unpremultiply methids to the Pixel class

--- lighten.py
+++ lighten.py
@@ -3,20 +3,20 @@ from inc.image_classes import Image
 image_one = Image("./images/red.png")
 image_two = Image("./images/blue_alpha.png")
 
-for pixel_a, pixel_b in zip(image_one.data(), image_two.data()):
+for background, foreground in zip(image_one.data(), image_two.data()):
 
     # Straight alpha lighten operation.
-    pixel_a.r = max(pixel_a.r * pixel_a.a, pixel_b.r * pixel_b.a)
-    pixel_a.g = max(pixel_a.g * pixel_a.a, pixel_b.g * pixel_b.a)
-    pixel_a.b = max(pixel_a.b * pixel_a.a, pixel_b.b * pixel_b.a)
+    background.r = max(background.r * background.a, foreground.r * foreground.a)
+    background.g = max(background.g * background.a, foreground.g * foreground.a)
+    background.b = max(background.b * background.a, foreground.b * foreground.a)
 
     # Premultiplied lighten operation.
-    pixel_a.premultiply()
-    pixel_b.premultiply()
-    pixel_a.r = max(pixel_a.r, pixel_b.r)
-    pixel_a.g = max(pixel_a.g, pixel_b.g)
-    pixel_a.b = max(pixel_a.b, pixel_b.b)
-    pixel_a.a = max(pixel_a.a, pixel_b.a)
+    background.premultiply()
+    foreground.premultiply()
+    background.r = max(background.r, foreground.r)
+    background.g = max(background.g, foreground.g)
+    background.b = max(background.b, foreground.b)
+    background.a = max(background.a, foreground.a)
 
 
 image_one.show(title="Lighten operation")
--- over.py
+++ over.py
@@ -3,19 +3,19 @@ from inc.image_classes import Image
 image_one = Image("./images/red.png")
 image_two = Image("./images/blue_alpha.png")
 
-for pixel_a, pixel_b in zip(image_one.data(), image_two.data()):
+for background, foreground in zip(image_one.data(), image_two.data()):
 
     # Straight alpha over operation.
-    pixel_a.r = (pixel_b.r * pixel_b.a) + (pixel_a.r * ( 1 - pixel_b.a))
-    pixel_a.g = (pixel_b.g * pixel_b.a) + (pixel_a.g * ( 1 - pixel_b.a))
-    pixel_a.b = (pixel_b.b * pixel_b.a) + (pixel_a.b * ( 1 - pixel_b.a))
+    background.r = (foreground.r * foreground.a) + (background.r * (1 - foreground.a))
+    background.g = (foreground.g * foreground.a) + (background.g * (1 - foreground.a))
+    background.b = (foreground.b * foreground.a) + (background.b * (1 - foreground.a))
 
     # Premultiplied over operation
-    pixel_a.premultiply()
-    pixel_b.premultiply()
-    pixel_a.r = pixel_b.r + pixel_a.r * (1 - pixel_b.a)
-    pixel_a.g = pixel_b.g + pixel_a.g * (1 - pixel_b.a)
-    pixel_a.b = pixel_b.b + pixel_a.b * (1 - pixel_b.a)
+    background.premultiply()
+    foreground.premultiply()
+    background.r = foreground.r + background.r * (1 - foreground.a)
+    background.g = foreground.g + background.g * (1 - foreground.a)
+    background.b = foreground.b + background.b * (1 - foreground.a)
 
 
 image_one.show(title="Over operation")
